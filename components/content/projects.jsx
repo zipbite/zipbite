77451df6418f247f1c7c39e98062f13b718a651f
Zipbyte <zipbite@protonmail.com>
2024-12-05 17:10:40 +0100

    Update projects.jsx

--- components/content/projects.jsx
+++ components/content/projects.jsx
@@ -13,6 +13,13 @@ export default function Projects() {
             </div>
             <div className="flex flex-col gap-2">
               <div className='flex items-center gap-2'>
+                <p className="text-zinc-100 font-semibold">Lain</p>
+                <span className='bg-zinc-900 rounded text-[0.65rem] uppercase font-bold tracking-wide px-3 py-0.5 text-amber-500'>In Development</span>
+              </div>
+              <p className="text-sm">Multipurpose discord bot with plenty of features.</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className='flex items-center gap-2'>
                 <p className="text-zinc-100 font-semibold">Animux</p>
                 <span className='bg-zinc-900 rounded text-[0.65rem] uppercase font-bold tracking-wide px-2 py-0.5 text-rose-500'>TBA</span>
               </div>
