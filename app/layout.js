4f71039b399a6f50d5d8e3be94bef87d9a1c1970
Zipbyte <zipbite@protonmail.com>
2024-12-05 07:24:22 +0100

    Add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "Zipbyte",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       >
         <main className="max-w-7xl mx-auto">
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
